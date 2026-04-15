8045d719d329080721e89ae393ae6d17e0390e4c
lifr0m <artem.kay@yandex.ru>
2026-04-16 01:12:22 +0300

    add examples

---
+++ durov-client/examples/bot.rs
@@ -0,0 +1,42 @@
+use durov_client::config::Config;
+
+type Client = durov_client::client::Client<
+    durov_mtproto::transports::full::Full,
+    durov_client::sessions::telethon::Telethon,
+>;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> eyre::Result<()> {
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::Warn)
+        .init();
+
+    let api_id = 0; // ENTER YOUR API ID
+    let api_hash = String::from("ENTER YOUR API HASH");
+    let mut config = Config::new(api_id, api_hash);
+    config.updates = true;
+    // Uncomment if you want to pull updates which occurred while bot was offline.
+    // config.catch_up = true;
+
+    let client = Client::connect("bot.session", config).await?;
+
+    // You can use `client.next_unauthorized_updates` to receive updates
+    // which happen on unauthorized connections, for example QR login.
+
+    loop {
+        tokio::select! {
+            // Rust doesn't have `AsyncDrop` trait (yet) so we have to manually save updates state.
+            _ = tokio::signal::ctrl_c() => {
+                client.save_updates().await?;
+                break;
+            }
+            updates = client.next_authorized_updates() => {
+                for update in updates? {
+                    println!("{update:?}");
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ durov-client/examples/userbot.rs
@@ -0,0 +1,27 @@
+use durov_client::config::Config;
+use durov_client::tl;
+
+type Client = durov_client::client::Client<
+    durov_mtproto::transports::full::Full,
+    durov_client::sessions::telethon::Telethon,
+>;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> eyre::Result<()> {
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::Warn)
+        .init();
+
+    let api_id = 0; // ENTER YOUR API ID
+    let api_hash = String::from("ENTER YOUR API HASH");
+    let config = Config::new(api_id, api_hash);
+
+    let client = Client::connect("userbot.session", config).await?;
+
+    let res = client.call(tl::functions::users::GetUsers {
+        id: vec![tl::types::InputUserSelf {}.into()],
+    }).await?;
+    println!("{res:#?}");
+
+    Ok(())
+}
